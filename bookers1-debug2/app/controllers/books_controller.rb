cae8eb2d527043574e5ed6a32f806e41e11e3df6
EC2 Default User <ec2-user@ip-172-31-30-244.ap-northeast-1.compute.internal>
2022-08-03 23:32:36 +0000

    [Finish] debug2

--- bookers1-debug2/app/controllers/books_controller.rb
+++ bookers1-debug2/app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,7 +28,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
--- bookers1-debug2/db/schema.rb
+++ bookers1-debug2/db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_07_27_061455) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
